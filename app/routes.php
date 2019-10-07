49528f7679224f23297fa8951a996e7d17c0a557
wand2016 <d19921207@gmail.com>
2019-10-07 20:44:17 +0900

    $routesのcommandは`[$status, $headers, $body]`を返すように

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,17 @@
 <?php
 
 $routes['/'] = function () {
+    ob_start();
     include __DIR__ . '/../app/view/index.phtml';
+
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
 $routes['/phpinfo.php'] = function () {
+    ob_start();
     phpinfo();
+
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
 return $routes;
--- public/index.php
+++ public/index.php
@@ -7,8 +7,15 @@ $routes = require __DIR__ .'/../app/routes.php';
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if (isset($routes[$uri])) {
-    $f = $routes[$uri]();
-    $f();
+    $f = $routes[$uri];
+    list($status, $headers, $body) = $f();
+
+    http_response_code($status);
+
+    foreach($headers as $name => $h) {
+        header("{$name}: {$h}");
+    }
+    echo $body;
 } else {
     http_response_code(404);
     echo '<h1>404 not found</h1>';
