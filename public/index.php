5ccf70f82e774d769d3047b10ee8ca81fdf155a0
wand2016 <d19921207@gmail.com>
2019-10-07 20:04:19 +0900

    phpinfo.php消した

--- public/index.php
+++ public/index.php
@@ -12,5 +12,10 @@ if ($_SERVER['REQUEST_URI'] === '/') {
     exit;
 }
 
+if ($_SERVER['REQUEST_URI'] == '/phpinfo.php') {
+    phpinfo();
+    exit;
+}
+
 http_response_code(404);
 echo '<h1>404 not found</h1>';
