e98196948222996876726fc013390ed068f63c34
wand2016 <d19921207@gmail.com>
2019-10-07 20:26:56 +0900

    テンプレートファイル分けた

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,7 @@
 <?php
 
 $routes['/'] = function () {
-    echo "<!DOCTYPE html>" . PHP_EOL;
-    echo "<title>test</title>" . PHP_EOL;
-    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>" . PHP_EOL;
-    echo h("<script>document.write('malicious code')</script>") . PHP_EOL;
-    echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>" . PHP_EOL;
+    include __DIR__ . '/../app/view/index.phtml';
 };
 
 $routes['/phpinfo.php'] = function () {
