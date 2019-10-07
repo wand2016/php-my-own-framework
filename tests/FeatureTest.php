2b1d9c17f37f947df3497ad67c5f5f541d94caee
wand2016 <d19921207@gmail.com>
2019-10-07 20:35:22 +0900

    テスト導入中

---
+++ tests/FeatureTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+
+class FeatureTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function rootにGETでアクセスして200OK()
+    {
+        $this->assertTrue(true);
+        // $client = new Client();
+
+        // $this->assertSame(
+        //     200,
+        //     $client->get('/')->getStatusCode()
+        // );
+
+    }
+
+}
