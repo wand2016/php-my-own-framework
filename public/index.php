1431578d971627ac854b8e1909c75882903ed156
wand2016 <d19921207@gmail.com>
2019-10-07 20:08:52 +0900

    はじめてのrouting

--- public/index.php
+++ public/index.php
@@ -3,19 +3,23 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-if ($_SERVER['REQUEST_URI'] === '/') {
+$routes['/'] = function () {
     echo "<!DOCTYPE html>" . PHP_EOL;
     echo "<title>test</title>" . PHP_EOL;
     echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>" . PHP_EOL;
     echo h("<script>document.write('malicious code')</script>") . PHP_EOL;
     echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>" . PHP_EOL;
-    exit;
-}
+};
 
-if ($_SERVER['REQUEST_URI'] == '/phpinfo.php') {
+$routes['/phpinfo.php'] = function () {
     phpinfo();
-    exit;
-}
+};
 
-http_response_code(404);
-echo '<h1>404 not found</h1>';
+
+if (isset($routes[$_SERVER['REQUEST_URI']])) {
+    $f = $routes[$_SERVER['REQUEST_URI']]();
+    $f();
+} else {
+    http_response_code(404);
+    echo '<h1>404 not found</h1>';
+}
