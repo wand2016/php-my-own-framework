2b09b046cdf2c65907cd98a166d8cd83b3b45331
wand2016 <d19921207@gmail.com>
2019-10-07 20:24:07 +0900

    クエリパラメータ対応

--- public/index.php
+++ public/index.php
@@ -4,8 +4,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $routes = require __DIR__ .'/../app/routes.php';
 
-if (isset($routes[$_SERVER['REQUEST_URI']])) {
-    $f = $routes[$_SERVER['REQUEST_URI']]();
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (isset($routes[$uri])) {
+    $f = $routes[$uri]();
     $f();
 } else {
     http_response_code(404);
